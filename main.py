02440268319dc05199d962a90e4348bb09d60a11
Sonya Burneyko <sonya_burneyko@mail.ru>
2020-09-05 15:37:32 +0300

    6a commit

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ GameDesk = [[" "] * 5 for i in range(4)]
 # GameDesk.append(['0', '0', '0', '0', '0'])
 # GameDesk.append(['0', '0', '0', '0', '0'])
 # wqeqweqwe
+# 32132
 # elkrjklskfjskdfhjkjshf
 <<<<<<< HEAD
 # iz doma
