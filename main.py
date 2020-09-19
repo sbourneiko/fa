688d9cec08732c6ad7f8ae9fcbf9809d69d31956
Dmitriy Bourneiko <dbourneiko@gmail.com>
2020-09-19 18:08:38 +0300

    msg

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# обявляем игровое поле как список
+# и заполняем список начальными данными
+GameDesk = [[" "] * 5 for i in range(4)]
+
+# объявляем список игроков
+Gamers = ["*", "+", "-"]
+Penalties = [0 ,0, 0]
+
+def getPenalty(gamer, i, k):
+    pen = 0
+    print(locals())
+    top = i - 1
+    left = k - 1
+    bottom = i + 1
+    right = k + 1
+
+    for y in range(top, bottom+1):
+        for x in range(left, right+1):
+    # if GameDesk[i][k] == '223321':
+            print(f"Соседние ячеки ({y}, {x}) : {GameDesk[y][x]}")
+    #     pen += 1
+    return 666
+
 def showGameDesk():
     # вывод игрового поля
     print("Игровое поле:")
@@ -13,33 +36,45 @@ def showGameDesk():
             print(f"{GameDesk[i][j]}|", end="")
         print()
 
-# объявляем список игроков
-Gamers = ["*", "+", "-"]
 
-# обявляем игровое поле как список
-# и заполняем список начальными данными
-GameDesk = [[" "] * 5 for i in range(4)]
-# GameDesk.append(['0', '0', '0', '0', '0'])
-# GameDesk.append(['0', '0', '0', '0', '0'])
-# GameDesk.append(['0', '0', '0', '0', '0'])
-# GameDesk.append(['0', '0', '0', '0', '0'])
-# wqeqweqwe
-# 32132
-# elkrjklskfjskdfhjkjshf
-# ewrwer
-# 1 univer
-showGameDesk()
+def inputPlayer(gamer):
+    # запрос хода
+    # выводим поле
+    showGameDesk()
 
-# 33333333333333333333333333333333
-# rewrewer
-# sdrdsfdsf
+    print(f"Игрок {gamer + 1}, вы ходите символом '{Gamers[gamer]}'")
+    print("Ваш ход, введите два числа!")
+    try:
+        i = int(input("По верикали: "))
+        k = int(input("По горизонтали: "))
+    except ValueError:
+        print("Ошибка! Введены неверные значения!")
+        return inputPlayer(gamer)
 
-showGameDesk()
+    try:
+        if GameDesk[i][k] != " ":
+            print("Ошибка! Клетка занята!")
+            (i, k) = inputPlayer(gamer)
+    except IndexError:
+        print("Ошибка! Ход вне поля!")
+        (i, k) = inputPlayer(gamer)
+
+    return (i, k)
+
+
 # привет
-i = 0
 while True:
-    for gamer in range(len(Gamers)):
-        print(f"Игрок {gamer+1}, вы ходите символом '{Gamers[gamer]}'")
-        (i, k) = input("Ваш ход (введите два числа):")
-        GameDesk[int(i)][int(k)] = Gamers[gamer]
-        showGameDesk()
+    for gamer_idx in range(len(Gamers)):
+        # запрашиваем ход
+        print("-" * 50)
+        (i, k) = inputPlayer(gamer_idx)
+        print(f"Вы ввели {i}, {k}")
+
+        # ставим ход на доску
+        GameDesk[i][k] = Gamers[gamer_idx]
+
+        # вычисляем штрафные очки
+        step_points = getPenalty(gamer_idx, i, k)
+        Penalties[gamer_idx] += step_points
+        print(f"Игрок {gamer_idx+1}, Вы получили штраф - {step_points} очков")
+        print(f"Сумма ваших штрафов = {Penalties[gamer_idx]}")
